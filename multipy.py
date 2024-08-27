34bfb29bafede75ce0dd4fd55c1b090e1d12a54e
Kimbeauty1 <ukpongdeborah44@gmail.com>
2024-08-27 13:02:18 +0100

    message

---
+++ multipy.py
@@ -0,0 +1,9 @@
+#function to multiply two numbers and display the outcome of the numbers
+
+def get_multiplication():
+    print("multiplication")
+    number1 = 8
+    number2 = 12    
+    multiplication = number1 * number2
+    print("the multiplication of", number1, "*", number2, "=", multiplication)
+get_multiplication()
